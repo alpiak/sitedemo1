3a699cebbdb1d880e654bf65133155aeb72db650
frontbee <hang3126@sina.com>
2016-07-15 07:12:54 +0800

    Add files via upload

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,1 +1,72 @@
+// name space
+if (typeof newbeef === "undefined") {
+    var newbeef = {};
+}
+// sub name spaces
+if (typeof newbeef.blogReader === "undefined") {
+    newbeef.blogReader = {};
+}
+if (typeof newbeef.blogGallery === "undefined") {
+    newbeef.blogGallery = {};
+}
 
+newbeef.setLayout = (function () {
+    var footer = document.getElementById("footer");
+    var _documentHeight = document.body.offsetHeight ||
+    (document.documentElement && (document.documentElement.offsetHeight ||
+    document.documentElement.scrollHeight));
+    
+    return function () {
+        // Set footer
+        if (_documentHeight < window.innerHeight) {
+            footer.style.position = "static";
+        } else {
+            footer.style.position = "fixed";
+        }
+     };
+}());
+
+newbeef.blogReader.resetLayout = (function () {
+    var seperator = document.getElementById("seperator");
+    return function () {
+        seperator.style.height = "0";
+    }
+}());
+
+newbeef.blogReader.setLayout = (function (resetLayout, setLayout) {
+    var menu = document.getElementById("menu");
+    var content = document.getElementById("content");
+    var seperator = document.getElementById("seperator");
+
+    return function () {
+        resetLayout();
+        setLayout();
+        // set seperator
+        if (content.offsetHeight > menu.offsetHeight) {
+            seperator.style.height = content.offsetHeight + "px";
+        } else {
+            seperator.style.height = menu.offsetHeight + "px";
+        }
+    }
+}(newbeef.blogReader.resetLayout, newbeef.setLayout));
+
+newbeef.blogGallery.setLayout = (function (setLayout) {
+    return function () {
+        var i = $(".blog-gallery-block").first();
+        (function () {
+            try {
+                i.find("p").css("width", i.find("img")[0].offsetWidth -
+                parseInt(i.find("p").css("padding"), 10) * 2 -
+                parseInt(i.find("p").css("margin"), 10) * 2 + "px");
+            } catch (err) {
+                i.find("p").css("width", "160px");
+            }
+            i.css("animation-play-state", "running");
+            i = i.next();
+            if (i.length != 0) {
+                window.setTimeout(arguments.callee, 100);
+            };
+            setLayout();
+        } ());
+    };
+} (newbeef.setLayout));
---
+++ scripts/load_blog_list.js
@@ -0,0 +1,61 @@
+if (typeof newbeef === "undefined") {
+    var newbeef = {};
+}
+if (typeof newbeef.blogReader === "undefined") {
+    newbeef.blogReader = {};
+}
+
+(function () {
+    newbeef.blogReader.loadBlogList =
+    function (sort, callback) {
+
+        var xmlhttp;
+        var blogs, blogid, ul;
+        var menulist = document.getElementById("menu-list");
+
+        try {
+            xmlhttp = new XMLHttpRequest();
+        } catch (tryMS) {
+            try {
+                xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (otherMS) {
+                try {
+                    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+                } catch (failed) {
+                    xmlhttp = null;
+                }
+            }
+        }
+
+        if (xmlhttp === null) {
+            menulist.innerHTML = "Unable to create the request";
+            return;
+        }
+
+        xmlhttp.onreadystatechange = stateChange;
+        xmlhttp.open("GET", "scripts/load_blog_list.php?t=" + Math.random() +
+    "&sort=" + sort, true);
+        xmlhttp.send();
+
+        // loading message
+        if (!document.getElementById("loading")) {
+            menulist.innerHTML = "<div id=\"loading\" style = \"position: absolute; margin: auto; \
+                            width: auto; height: 20px; line-height: 20px; text-align: center; \
+                            top: 0; right: 0; bottom: 0; left: 0\">Loading...</div>";
+        }
+
+        function stateChange() {
+            var readystate = xmlhttp.readyState;
+
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200 && xmlhttp.responseText) {
+                menulist.removeChild(loading);
+                txt = xmlhttp.responseText;
+                menulist.innerHTML = txt;
+                if (callback) {
+                    callback();
+                }
+            }
+        }
+
+    }
+}());
