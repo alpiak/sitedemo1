104a0c300eb0ffddd90aa3b89f72194cf62242d3
frontbee <hang3126@sina.com>
2016-08-07 17:17:46 +0800

    Add files via upload
    feature improvement

--- scripts/set_up_image_slidebox.js
+++ scripts/set_up_image_slidebox.js
@@ -8,16 +8,26 @@ if (typeof newbeef === undefined) {
 		var active = imageul.getElementsByClassName("active")[0].getAttribute("data-imgid");
 		var left = -450 * active; // controls the position of the image ul
 		var lastActive, lastLeft; // remember the last active image index
+		var activeDiffer // active - lastActive
+		var cloneLeft; // for the cloned images
 		
-		function onTrigger() {
-			if (Math.abs(active - lastActive) < 4) {
+		function activeApply() {
+			activeDiffer = active - lastActive;
+			if (Math.abs(activeDiffer) < 4) {
 				imageul.style.left = left + "px";
 			} else {
-				imageul.style.left = lastLeft - 450 + "px";
+				if (activeDiffer < 0) {
+					cloneLeft = lastLeft - 450;
+				} else {
+					cloneLeft = lastLeft + 450;
+				}
+				imageul.style.left = cloneLeft + "px";
+				imageul.className += "onclone";
+				
+				// switch from the cloned image to the real image
 				setTimeout(function () {
 					var interval;
 					
-					imageul.className += "onclone";
 					imageul.style.left = left + "px";
 					interval = setInterval(function () {
 						if (imageul.style.left === left + "px") {
@@ -29,18 +39,42 @@ if (typeof newbeef === undefined) {
 			}
 		}
 		
+		function activeIncrease () {
+			lastActive = active;
+			if (active < 4) {
+				active++;
+			} else {
+				active = 0;
+			}
+			left = -450 * active;
+			lastLeft = -450 * lastActive;
+		}
+		
+		function activeDecrease () {
+			lastActive = active;
+			if (active > 0) {
+				active--;
+			} else {
+				active = 4;
+			}
+			left = -450 * active;
+			lastLeft = -450 * lastActive;
+		}
+		
 		return function () {
 			var interval = setInterval(function () {
-				lastActive = active;
-				if (active < 4) {
-					active++;
-				} else {
-					active = 0;
-				}
-				left = -450 * active;
-				lastLeft = -450 * lastActive;
-				onTrigger();
+				activeIncrease();
+				activeApply();
 			}, 3000);
+			
+			document.getElementById("slide-previous").onclick = function () {
+				activeDecrease();
+				activeApply();
+			}
+			document.getElementById("slide-next").onclick = function () {
+				activeIncrease();
+				activeApply();
+			}
 		}
 	}());
 }());
